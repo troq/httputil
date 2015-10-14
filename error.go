893cc417fe405be7db534e36f5917428c3f7e6f4
Tieshun Roquerre <tieshun.roquerre@gmail.com>
2015-10-13 21:06:30 -0700

    Add error functions that take a string

--- error.go
+++ error.go
@@ -6,6 +6,13 @@ func Error400(w http.ResponseWriter, err error) {
 	http.Error(w, err.Error(), http.StatusBadRequest)
 }
 
+func Error400S(w http.ResponseWriter, err string) {
+	http.Error(w, err, http.StatusBadRequest)
+}
+
 func Error500(w http.ResponseWriter, err error) {
 	http.Error(w, err.Error(), http.StatusInternalServerError)
+}
+func Error500S(w http.ResponseWriter, err string) {
+	http.Error(w, err, http.StatusInternalServerError)
 }
